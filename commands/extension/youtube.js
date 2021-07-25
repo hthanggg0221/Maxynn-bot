f9e7c44dbd1f07f2e19eb871b3bf4d5edc730453
maxynn212 <71207161+maxynn212@users.noreply.github.com>
2021-07-25 08:19:56 +0700

    Add files via upload

---
+++ commands/extension/youtube.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'youtube',
+    category: '<:Extension:868660497795395624> Extension',
+    aliases: ["ytb"],
+    description: 'Tạo 1 Rich Presence về ứng dụng Youtube Together',
+    usage: youtube,
+    run: (client, message,args) => {
+
+    }
+}
