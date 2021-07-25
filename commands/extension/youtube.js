15f7716f69bc3a4561f9af652384692fd7c9f379
maxynn212 <71207161+maxynn212@users.noreply.github.com>
2021-07-25 08:59:00 +0700

    Update youtube.js

--- commands/extension/youtube.js
+++ commands/extension/youtube.js
@@ -1,10 +1,38 @@
+const fetch = require('node-fetch');
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'youtube',
     category: '<:Extension:868660497795395624> Extension',
     aliases: ["ytb"],
     description: 'Tạo 1 Rich Presence về ứng dụng Youtube Together',
     usage: youtube,
-    run: (client, message,args) => {
+    run: (client, message, args) => {
+        if (!message.content.startsWith(process.env.PREFIX)) return;
 
+        let channel = message.member.voice.channel;
+        if(!channel) return message.reply('Bạn phải ở trong phòng trước khi sử dụng lệnh này!');
+
+        fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+            method: 'POST',
+            body: JSON.stringify({
+                max_age: 86400,
+                max_uses: 0,
+                target_application_id: "755600276941176913",
+                target_type: 2,
+                temporary: false,
+                validate: null
+            }),
+            headers: {
+                "Authorization": `Bot ${process.env.TOKEN}`,
+                "Content-Type": "application/json"
+            }
+        }).then(res => res.json()).then(invite => {
+            if(!invite.code) return message.channel.send('Đáng buồn thay, tôi không thể bắt đầu Rich Presence Youtube Together')
+            const ytbembed = new MessageEmbed()
+                .setAuthor('Lời mời sử dụng Youtube Together')
+                .setDescription(`Nhấn vào [link này](https://discord.com/invite/${invite.code}) để bắt đầu xem Youtube cùng nhau tại phòng ${channel}`)
+            message.channel.send(ytbembed);
+        })
     }
 }
