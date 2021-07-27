9452aaeb363f85cc42eb4b44f00c738dcfc17872
maxynn212 <71207161+maxynn212@users.noreply.github.com>
2021-07-27 19:04:36 +0700

    Add files via upload

---
+++ events/messageDelete.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require('discord.js');
+const { owner, logchannel } = require('../config.json');
+
+module.exports = {
+    name: 'messageDelete',
+    on: true,
+    run: async(message, client) => {
+        const Maxynn = await client.users.fetch(owner);
+        const logchannels = await client.channels.fetch(logchannel);
+        const deleteembed = new MessageEmbed()
+            .setColor('0xe91e63')
+            .setTimestamp()
+            .setFooter(`ID người gửi: ${message.author.id} | ID tin nhắn: ${message.id}`)
+            .setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL({ dynamic: true})}`)
+            .setDescription(`**Tin nhắn của** ${message.author} **đã bị xóa ở kênh** ${message.channel}\nNội dung: ${message.content}`);
+        logchannels.send(deleteembed);
+    }
+}
