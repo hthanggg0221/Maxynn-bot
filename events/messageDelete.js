96fa64da212a70838efa742a96da8517d2e686f8
maxynn212 <71207161+maxynn212@users.noreply.github.com>
2021-08-22 17:52:11 +0700

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -5,8 +5,7 @@ module.exports = {
     name: 'messageDelete',
     on: true,
     run: async(message, client) => {
-        if (message.author.bot) return;
-        if (!message.guild) return;
+        if (message.author.bot || !message.guild) return;
         const Maxynn = await client.users.fetch(owner);
         const logchannels = await client.channels.fetch(logchannel);
         const deleteembed = new MessageEmbed()
