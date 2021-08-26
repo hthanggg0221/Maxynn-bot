a3145eb35781109768109c925f913cd0d25ccd9d
maxynn212 <71207161+maxynn212@users.noreply.github.com>
2021-08-26 09:01:46 +0700

    Add files via upload

---
+++ commands/fun/confession.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require('discord.js');
+const { cfschannelevent } = require('../../config.json');
+const { getMember, promptMessage } = require('../../functions');
+const picExt = [".webp", ".png", ".jpg", ".jpeg", ".gif"];
+const videoExt = [".mp4", ".webm", ".mov"];
+
+module.exports = {
+    name: 'confessionevent',
+    category: '<:DiscordController:810683447001481226> Fun',
+    aliases: ['cfs'],
+    description: 'Nói một câu nào đó qua bot',
+    usage: 'cfs <nội dung>',
+    run: async (client, message, args) => {
+        const tim = client.emojis.cache.get("810675876135829524");
+        if (message.length > 1024) return message.channel.send('Confession chỉ được dưới 1024 kí tự');
+        else {
+            await message.react('👀');
+            message.delete();
+            
+            const cfsChannel = client.channels.cache.get(cfschannelevent);
+            if (!cfsChannel) return;
+            const embed = new MessageEmbed()
+                .setColor('0xb076c9')
+                .setTimestamp()
+                .setDescription(`${args.join(" ")}`)
+                .setFooter(`Được gửi bởi (${message.author.id})`)
+                .setTitle(`${message.guild.name} Confession`);
+            if (message.attachments.array().length > 0) {
+                let attachment = message.attachments.array()[0];
+                picExt.forEach(ext => {
+                    if (attachment.name.endsWith(ext)) embed.setImage(attachment.attachment);
+                });
+                videoExt.forEach(ext => {
+                    if (attachment.name.endsWith(ext)) cfsChannel.send(attachment);
+                });
+            };
+            await cfsChannel.send(embed).then(async msg => {
+            const emoji = await promptMessage(msg, message.author, 30, [tim]);
+            });
+        }
+    }
+}
